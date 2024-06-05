aab4e0fc0c40ce973998caeace5d3fdd4c814980
Yash Walke <142409974+Yash-6503@users.noreply.github.com>
2024-06-05 10:59:51 +0000

    Variables and Constants

--- Basics/variables.js
+++ Basics/variables.js
@@ -6,13 +6,23 @@ let y = 6;
 let z = x + y;
 console.log(z);
 
-//Variables are declared with the var keyword as well as let and const.
+//Variables are declared with the let keyword as well as const.
+//A constant is a variable whose value cannot be changed once it is assigned.
 
 const accountId = 123456;
 let accountName = "John Doe";
 accountBalance = 1000.00;
-let accoutCity = "Nashik";
+let accountCity = "Nashik";
 
-console.table[accountId, accountName, accountBalance, accoutCity];
+// accountId = 98765;       Constant Variables value cannot change once it is declared.
+accountName = "John Smith";
+accountBalance = 2000.00;
+accountCity = "Pune";
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountBalance);
+console.log(accountCity);
+console.table[accountId, accountName, accountBalance, accountCity];
 
 
