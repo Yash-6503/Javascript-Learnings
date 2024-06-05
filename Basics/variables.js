59c75575331cb1492334dce318df519156114c00
Yash-6503 <yashwalke6503@gmail.com>
2024-06-05 16:20:52 +0530

    Variables and Constants

---
+++ Basics/variables.js
@@ -0,0 +1,18 @@
+//Variables in Javascript
+//Variables are containers for storing data values.
+//In this example, x, y, and z are variables:
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z);
+
+//Variables are declared with the var keyword as well as let and const.
+
+const accountId = 123456;
+let accountName = "John Doe";
+accountBalance = 1000.00;
+let accoutCity = "Nashik";
+
+console.table[accountId, accountName, accountBalance, accoutCity];
+
+
