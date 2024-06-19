d9bca15fc43a77b56c4dc2ea5a89e5e07365c10a
Yash-6503 <yashwalke6503@gmail.com>
2024-06-19 08:52:02 +0530

    Strings in Javascript

---
+++ Practice/strings.js
@@ -0,0 +1,10 @@
+//Strings in javascript
+
+let str = "Hello World";
+console.log(str);
+console.log(str.length);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.charAt(0));
+console.log(str.charAt(6));
+console.log(str.charAt(10));
