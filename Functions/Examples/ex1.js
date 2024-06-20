78f59ed5bba3c1bf2e7de71e90e0947f37958882
Yash-6503 <yashwalke6503@gmail.com>
2024-06-20 09:38:14 +0530

    example of shopping cart total

---
+++ Functions/Examples/ex1.js
@@ -0,0 +1,8 @@
+//example of shoping cart with functions
+//to calculate the total price of the items in the cart
+
+function CalculateCart(...num) {
+    return num;
+}
+
+console.log(CalculateCart(100, 500, 2000, 5000));
